d93d4608167a712c5baacc9b7823683ceef17d17
nghinguyen@dede.finance <nghinguyen@dede.finance>
2022-09-25 17:37:16 +0700

    Init project with basic set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from '../themes';
+import Layout from '../components/layout';
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }: AppProps) {
+	const [theme, setTheme] = useState('light');
+
+	const toggleTheme = () => {
+		theme == 'light' ? setTheme('dark') : setTheme('light');
+	};
+
+	return (
+		<>
+			<GlobalStyles theme={theme == 'light' ? lightTheme : darkTheme} />
+			<ThemeProvider theme={theme == 'light' ? lightTheme : darkTheme}>
+				<Layout router={router} toggleTheme={toggleTheme}>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
